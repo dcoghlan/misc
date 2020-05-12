e73ea4019b137b8b21fedcbb5c461459979478ca
Dale Coghlan <dcoghlan@dcoghlan-a02.vmware.com>
2020-05-12 22:20:46 +1000

    Added add-vms-to-policy-groups script

---
+++ NSX-T/add-vms-to-policy-groups/README.md
@@ -0,0 +1,67 @@
+# Add Virtual Machines to Pre-Created NSX-T Policy Groups
+
+This script has been written with the purpose of being used as part of a suite of tools to help in the migration process from a NSX-v to NSX-T environment.
+
+The script will process some pre-created desired state policy json files which contain `virtualmachine/external_id` expressions, and add them to the relevant groups in NSX-T once the external_id of the VM is found in the NSX-T inventory.
+
+The script requires the following inputs:
+
+- NSX-T Manager IP/FQDN
+- NSX-T Manager API Username
+- NSX-T Manager API Password
+- Path to the directory containing the pre-created desired state json files.
+
+## Example
+
+``` bash
+PS > ./add-vms-to-policy-groups.ps1 -NsxManager 192.168.172.28 -Username admin -password VMware1!VMware1! -JsonDirectory './jsonFiles/'
+
+
+  --> Retrieving inventory virtual machines from NSX Manager
+  --> Processing: securitygroup-71 (All Legacy Machines)...UPDATED (Some external_ids missing, check log file)
+
+Execution completed in 0:0:2
+LogFile: add-vms-to-policy-groups_2020-05-12-221026.log
+
+```
+
+The status next to each group being processed is as follows:
+
+- SKIPPED = No changes made to the group, all the external_id/s specificed in the file were not visibile in the NSX-T inventory at the time the script was run.
+- FAILED = An interaction with the NSX Manager failed. This will be because of failure to retrieve the group object, or patch the object. Check the log file for more information.
+- OK = The specified external_ids already exists in the expression. If at least one is still missing as it wasn't found in the NSX-T inventory, it will display `OK (Some external_ids missing, check log file)`
+- UPDATED = The object has been updated with at least 1 or more valid external_ids. If at least one is still missing as it wasn't found in the NSX-T inventory, it will display `OK (Some external_ids missing, check log file)`
+
+It is safe to run the script multiple times.  If there are no changes to apply then nothing will be changed. It will only add VMs as it finds them. So it should be safe to continually run the script untill it reports all groups with a status of `OK`.
+
+## LogFile Output
+
+``` log
+22:10:26 : add-vms-to-policy-groups.ps1: line 316, add-vms-to-policy-groups.ps1: INFO: --------------------------------------------------------------------------------
+22:10:26 : add-vms-to-policy-groups.ps1: line 317, add-vms-to-policy-groups.ps1: INFO: Script start time = 05/12/2020 22:10:26
+22:10:26 : add-vms-to-policy-groups.ps1: line 320, add-vms-to-policy-groups.ps1: VERBOSE: JsonDirectory = ./jsonFiles/
+22:10:26 : add-vms-to-policy-groups.ps1: line 321, add-vms-to-policy-groups.ps1: VERBOSE: groupFileIdentifier = _group_
+22:10:26 : add-vms-to-policy-groups.ps1: line 322, add-vms-to-policy-groups.ps1: VERBOSE: NSX Manager = 192.168.172.28
+22:10:26 : add-vms-to-policy-groups.ps1: line 323, add-vms-to-policy-groups.ps1: VERBOSE: NSX Manager Username = admin
+22:10:26 : add-vms-to-policy-groups.ps1: line 324, add-vms-to-policy-groups.ps1: VERBOSE: Powershell Edition: Core
+22:10:26 : add-vms-to-policy-groups.ps1: line 325, add-vms-to-policy-groups.ps1: VERBOSE: Powershell Version: 7.0.0
+22:10:26 : add-vms-to-policy-groups.ps1: line 326, add-vms-to-policy-groups.ps1: VERBOSE: Powershell OS: Darwin 19.4.0 Darwin Kernel Version 19.4.0: Wed Mar  4 22:28:40 PST 2020; root:xnu-6153.101.6~15/RELEASE_X86_64
+22:10:26 : add-vms-to-policy-groups.ps1: line 327, add-vms-to-policy-groups.ps1: VERBOSE: Powershell Platform: Unix
+22:10:27 : add-vms-to-policy-groups.ps1: line 371, add-vms-to-policy-groups.ps1: INFO: --------------------------------------------------------------------------------
+22:10:27 : add-vms-to-policy-groups.ps1: line 372, add-vms-to-policy-groups.ps1: VERBOSE: Loading _group_ file: SAMPLE_group_All Legacy Machines.json
+22:10:27 : add-vms-to-policy-groups.ps1: line 376, add-vms-to-policy-groups.ps1: VERBOSE: securitygroup-71: Processing group: All Legacy Machines
+22:10:27 : add-vms-to-policy-groups.ps1: line 377, add-vms-to-policy-groups.ps1: VERBOSE: securitygroup-71: Total Expressions found = 1
+22:10:27 : add-vms-to-policy-groups.ps1: line 383, add-vms-to-policy-groups.ps1: VERBOSE: securitygroup-71: Processing external_id found in _group_ file: 5018eb82-2d36-d637-59d4-5a7bd01c76bd
+22:10:27 : add-vms-to-policy-groups.ps1: line 386, add-vms-to-policy-groups.ps1: ERROR: securitygroup-71: External_id not found in NSX-T Inventory: 5018eb82-2d36-d637-59d4-5a7bd01c76bd
+22:10:27 : add-vms-to-policy-groups.ps1: line 383, add-vms-to-policy-groups.ps1: VERBOSE: securitygroup-71: Processing external_id found in _group_ file: 5023db4b-f567-4e6b-42e1-de7c781e20fe
+22:10:27 : add-vms-to-policy-groups.ps1: line 391, add-vms-to-policy-groups.ps1: VERBOSE: securitygroup-71: Found matching external_id in NSX-T Inventory: 5023db4b-f567-4e6b-42e1-de7c781e20fe (DR-App-01)
+22:10:27 : add-vms-to-policy-groups.ps1: line 383, add-vms-to-policy-groups.ps1: VERBOSE: securitygroup-71: Processing external_id found in _group_ file: 5018be9b-781d-e7ad-1fbc-4e8a630b29ed
+22:10:27 : add-vms-to-policy-groups.ps1: line 386, add-vms-to-policy-groups.ps1: ERROR: securitygroup-71: External_id not found in NSX-T Inventory: 5018be9b-781d-e7ad-1fbc-4e8a630b29ed
+22:10:27 : add-vms-to-policy-groups.ps1: line 402, add-vms-to-policy-groups.ps1: VERBOSE: securitygroup-71: Additional external_ids to add = 1
+22:10:27 : add-vms-to-policy-groups.ps1: line 404, add-vms-to-policy-groups.ps1: VERBOSE: securitygroup-71: Retrieving group configuration
+22:10:28 : add-vms-to-policy-groups.ps1: line 436, add-vms-to-policy-groups.ps1: VERBOSE: securitygroup-71: No existing VirtualMachine/ExternalIDExpression found
+22:10:28 : add-vms-to-policy-groups.ps1: line 441, add-vms-to-policy-groups.ps1: VERBOSE: securitygroup-71: Adding Conjunction with OR operator
+22:10:28 : add-vms-to-policy-groups.ps1: line 446, add-vms-to-policy-groups.ps1: VERBOSE: securitygroup-71: Adding new VirtualMachine/ExternalIDExpression
+22:10:28 : add-vms-to-policy-groups.ps1: line 456, add-vms-to-policy-groups.ps1: INFO: securitygroup-71: Patching updated configuration
+22:10:29 : add-vms-to-policy-groups.ps1: line 492, add-vms-to-policy-groups.ps1: INFO: Script duration = 00:00:03.4010980
+```
---
+++ NSX-T/add-vms-to-policy-groups/add-vms-to-policy-groups.ps1
@@ -0,0 +1,494 @@
+
+param (
+    [parameter(Mandatory = $True)]
+    # NSX Manager IP or FQDN.
+    [ValidateNotNullOrEmpty()]
+    [string] $NsxManager,
+    [parameter(Mandatory = $True)]
+    # Username used to authenticate to NSX API
+    [ValidateNotNullOrEmpty()]
+    [string] $Username,
+    [parameter(Mandatory = $True)]
+    # Password used to authenticate to NSX API
+    [ValidateNotNullOrEmpty()]
+    [string] $Password,
+    [parameter(Mandatory = $True)]
+    # Password used to authenticate to NSX API
+    [ValidateScript ( { if ( -not (test-path $_) ) { throw "Path containing the JSON files $_ does not exist." } else { $true } })]
+    [string] $JsonDirectory
+)
+
+# ------------------------------------------------------------------------------
+# No need to modify anything below this line.
+# ------------------------------------------------------------------------------
+#Requires -Version 5.1
+$groupFileIdentifier = "_group_"
+$pathSeparator = [IO.Path]::DirectorySeparatorChar # This is to determine dynamic path separators
+# ------------------------------------------------------------------------------
+# Functions
+# ------------------------------------------------------------------------------
+
+function _init {
+
+    if ( $psversiontable.psedition -eq "Desktop" ) {
+        # Add TLS1.2 support
+        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12, [Net.SecurityProtocolType]::Tls, [Net.SecurityProtocolType]::Ssl3
+
+        ## Define class required for certificate validation override.  Version dependant.
+        ## For whatever reason, this does not work when contained within a function?
+        $TrustAllCertsPolicy = @"
+    using System.Net;
+    using System.Security.Cryptography.X509Certificates;
+    public class TrustAllCertsPolicy : ICertificatePolicy {
+        public bool CheckValidationResult(
+            ServicePoint srvPoint, X509Certificate certificate,
+            WebRequest request, int certificateProblem) {
+            return true;
+            }
+        }
+"@
+    
+        if ( -not ("TrustAllCertsPolicy" -as [type])) {
+            add-type $TrustAllCertsPolicy
+        }
+    
+        $script:originalCertPolicy = [System.Net.ServicePointManager]::CertificatePolicy
+    }
+}
+
+function Invoke-NsxtRestMethod {
+
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidDefaultValueSwitchParameter","")]
+
+    param (
+        [parameter(Mandatory = $True)]
+        [ValidateSet("get", "put", "post", "delete", "patch")]
+        [string]$method,
+        [parameter(Mandatory = $True)]
+        [string]$uri,
+        [parameter(Mandatory = $false)]
+        [hashtable]$headers = @{ },
+        [parameter(Mandatory = $False)]
+        [switch]$SkipCertificateCheck = $True,
+        [parameter(Mandatory = $false)]
+        [string]$body
+    )
+
+    if ($psversiontable.psedition -eq "Desktop") {
+        #Use splatting to build up the IWR params
+        $irmSplat = @{
+            "method"  = $method;
+            "headers" = $headers;
+            "uri"     = $Uri;
+        }
+
+        if ( $PsBoundParameters.ContainsKey('Body')) {
+            $irmSplat.Add("body", $body)
+        }
+
+        if (( -not $ValidateCertificate) -and ([System.Net.ServicePointManager]::CertificatePolicy.tostring() -ne 'TrustAllCertsPolicy')) {
+            #allow untrusted certificate presented by the remote system to be accepted
+            [System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+        }
+        Write-Debug $uri
+        invoke-RestMethod @irmSplat
+    }
+    else {
+        #Use splatting to build up the IWR params
+        $irmSplat = @{
+            "method"  = $method;
+            "headers" = $headers;
+            "uri"     = $Uri;
+        }
+
+        if ( $PsBoundParameters.ContainsKey('Body')) {
+            $irmSplat.Add("body", $body)
+        }
+        
+        if ($PSBoundParameters.ContainsKey('SkipCertificateCheck')) {
+            $irmSplat.Add("SkipCertificateCheck", $SkipCertificateCheck)
+        }
+        Write-Debug $uri
+        Invoke-RestMethod @irmSplat
+    }
+}
+
+function Start-Log {
+    param (
+        [string]$file,
+        [switch]$overwrite = $False
+    )
+    # Create the output files if they don't already exist
+    if ( -not ( test-path $file )) {
+        write-verbose "$file not found... creating a new one"
+        New-Item -Type file $file | out-null
+        if ( test-path $file ) {
+            write-verbose "file $file created"
+        }
+    }
+    # If the file already exists, then if the OverwriteLogFile is specified, we
+    # remove the existing one and create a new log file
+    elseif ($overwrite -eq $True) {
+        Get-Item $file | Remove-Item
+        New-Item -Type file $file | out-null
+        Write-Log -Level Debug -Msg "New Logfile created as OverwriteLogFile was enabled."
+    }  
+}
+
+function Write-Log {
+    param (
+        [Parameter(Mandatory = $false)]
+        [ValidateSet("info", "host", "warning", "verbose", "debug", "error")]
+        [string]$level = "host",
+        [Parameter(Mandatory = $false)]
+        [ValidateNotNullOrEmpty()]
+        [ValidateSet("white", "yellow", "red", "magenta", "cyan", "green")]
+        [string]$ForegroundColor = "white",
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [object]$msg,
+        [Parameter(Mandatory = $False)]
+        [ValidateNotNullOrEmpty()]
+        [string]$file = $Script:logFilename
+    )
+
+    $ForegroundColor = "white"
+    $msgPrefix = "$(get-date -f "HH:mm:ss") : $((get-pscallstack)[1].Location), $((get-pscallstack)[1].Command):"
+
+    switch ($level) {
+        "error" {
+            Add-content -path $Script:logFilename -value "$msgPrefix ERROR: $msg"
+        }
+        "debug" {
+            write-debug "$msgPrefix $msg"
+            Add-content -path $Script:logFilename -value "$msgPrefix DEBUG: $msg"
+        }
+        "warning" {
+            write-warning "$msgPrefix $msg"
+            Add-content -path $Script:logFilename -value "$msgPrefix WARNING: $msg"
+        }
+        "verbose" {
+            write-verbose "$msgPrefix $msg"
+            Add-content -path $Script:logFilename -value "$msgPrefix VERBOSE: $msg"
+        }
+        "info" {
+            Write-Information "$msgPrefix $msg"
+            Add-content -path $Script:logFilename -value "$msgPrefix INFO: $msg"
+        }
+        "file" {
+            Add-content -path $Script:logFilename -value "$msgPrefix FILE: $msg"
+        }
+        default {
+            write-host "$msg" -ForegroundColor $ForegroundColor
+            Add-content -path $Script:logFilename -value "$msgPrefix $msg"
+        }
+    }
+}
+
+function Get-NsxtPolicyInventoryVm {
+
+    param (
+        [Parameter(Mandatory = $False)]
+        [String] $EnforcementPointId = "default"
+    )
+
+    $uri = New-Object System.UriBuilder("https://$nsxManager")
+
+    $uri.path = "/policy/api/v1/infra/realized-state/enforcement-points/$EnforcementPointId/virtual-machines"
+
+    try {
+        $response = Invoke-NsxtRestMethod -Method GET -URI $uri -Headers $script:headers -SkipCertificateCheck
+    }
+    catch {
+        throw ($_)
+    }
+
+    $response.results
+
+}
+
+function Get-NsxtPolicyGroup {
+    param (
+        [Parameter(Mandatory = $False)]
+        [String] $DomainId = "default",
+        [Parameter(Mandatory = $False)]
+        [String] $GroupId
+    )
+
+    $uri = New-Object System.UriBuilder("https://$nsxManager")
+
+    $uri.path = "/policy/api/v1/infra/domains/$DomainId/groups"
+
+    if ($PSBoundParameters.ContainsKey('GroupId')) {
+        $uri.path = $uri.path + "/$groupId"
+    }
+
+    try {
+        $response = Invoke-NsxtRestMethod -Method GET -URI $uri -Headers $script:headers -SkipCertificateCheck
+    }
+    catch {
+        throw ($_)
+    }
+
+    if ($response.results) {
+        $response.results
+    }
+    else {
+        $response
+    }
+}
+
+function Get-VmFromInventory {
+    param (
+        [string]$DisplayName,
+        [string]$ExternalId,
+        [object]$Inventory
+    )
+
+    if ($PSBoundParameters.ContainsKey('DisplayName')) {
+        return ($Inventory | Where-Object {$_.display_name -eq $DisplayName})
+    }
+    else {
+        return $Inventory | Where-Object {$_.external_id -eq $ExternalId}
+    }
+}
+
+function New-GroupConjunctionExpression {
+    param (
+        [string]$operator
+    )
+    $object = [ordered]@{
+        "conjunction_operator" = $operator.ToUpper();
+        "resource_type"     = "ConjunctionOperator";
+        "marked_for_delete" = $false;
+    }
+
+    $object
+}
+
+function New-GroupVmExpression {
+    param (
+        [string[]]$Id
+    )
+    $object = [ordered]@{
+        "member_type" = "VirtualMachine"
+        "resource_type"     = "ExternalIDExpression";
+        "marked_for_delete" = $false;
+        "external_ids" = New-Object System.Collections.ArrayList;
+    }
+
+    foreach ($item in $Id) {
+        $object.external_ids.Add($item) | Out-Null
+    }
+
+    $object
+}
+
+function Invoke-PatchPolicyObject {
+    param (
+        [parameter(Mandatory = $True)]
+        [object] $path,
+        [parameter(Mandatory = $True)]
+        [string] $body
+    )
+
+    $uri = New-Object System.UriBuilder("https://$nsxManager/policy/api/v1")
+    $uri.path += $path
+
+    write-debug $path
+    write-debug $body
+    Invoke-NsxtRestMethod -method PATCH -body $body -uri $uri -SkipCertificateCheck -Headers $headers
+
+}
+
+# ------------------------------------------------------------------------------
+# Execution
+# ------------------------------------------------------------------------------
+$StartTime = Get-Date
+
+# Run the init function. This sets up the POSH session to allow connectivity to
+# the NSX Manager
+_init
+
+$ScriptName = Get-Item ($MyInvocation.MyCommand.Name) | Select-Object -ExpandProperty BaseName
+$script:logFileName = "$($ScriptName)_$($StartTime.ToString('yyy-MM-dd-HHmmss')).log"
+Start-Log -File $Script:logFileName
+Write-Log -Level Info -Msg ('-'*80)
+Write-Log -Level Info -Msg "Script start time = $StartTime"
+
+# Log some interesting stuff
+Write-Log -Level Verbose -Msg "JsonDirectory = $JsonDirectory"
+Write-Log -Level Verbose -Msg "groupFileIdentifier = $groupFileIdentifier"
+Write-Log -Level Verbose -Msg "NSX Manager = $NsxManager"
+Write-Log -Level Verbose -Msg "NSX Manager Username = $Username"
+Write-Log -Level Verbose -Msg "Powershell Edition: $($psversiontable.PSEdition)"
+Write-Log -Level Verbose -Msg "Powershell Version: $(($psversiontable.PSVersion).ToString())"
+Write-Log -Level Verbose -Msg "Powershell OS: $($psversiontable.OS)"
+Write-Log -Level Verbose -Msg "Powershell Platform: $($psversiontable.Platform)"
+
+# This is the color of the log file output at the completion of the script.
+# We change this to Red if there are any issue encountered during the sctipt
+$completionColor = "Green"
+
+# Create the custom header for authentication
+$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $Username, $Password)))
+$script:headers = @{
+    "Authorization" = ("Basic {0}" -f $base64AuthInfo);
+    "Content-Type"  = "application/json"
+}
+
+# Retrieve the list of VMs in the inventory. We use this to see if the 
+# VM/external_id is visible to NSX Manager so the external_id can be added to 
+# the policy group.
+Write-Host "`n  --> Retrieving inventory virtual machines from NSX Manager"
+try {
+    $inventoryVms = Get-NsxtPolicyInventoryVm
+}
+Catch {
+    Write-Log -Level Error -Msg "Failed to retrieve inventory virtual machines from NSX Manager."
+    Write-Log -Level Error -Msg $_
+    Write-Host -foregroundcolor Red "  --> Error written to LogFile: $Script:LogFileName`n"
+    throw $_
+}
+
+# Generate a list of group files based on the file identifier from the directory specified.
+$groupJsonFiles = Get-ChildItem -Path $JsonDirectory | Where-Object {$_.name -match $groupFileIdentifier}
+
+# Process each NSX-T Policy Group json file. The configuration in the file is 
+# the desired state of the group, however as the VMs didn't exist in the NSX-T
+# inventory when the group was created, if we added them via the API (which 
+# suprisingly allows you do add an external_id that doesn't exist), it results
+# in the group having realization errors, along with every parent/nested group 
+# and any firewall rules which use the group.
+#
+# So we go through and pull out all the virtual_machine external_ids, and then
+# check the list of inventory vms from NSX Manager, and only if the external_id
+# is seen in NSX Manager do we then add the external_id to the policy group. 
+# This allows for a slow/staged migration.
+
+foreach ($item in $groupJsonFiles) {
+    $missingExternalIds = $False
+    Write-Log -Level Info -Msg ('-'*80)
+    Write-Log -Level Verbose -Msg "Loading $groupFileIdentifier file: $($item.name)"
+
+    $json = Get-Content -path $JsonDirectory$pathSeparator$($item.name) | ConvertFrom-Json
+    Write-host -nonewline "  --> Processing: $($json.id) ($($json.display_name))..."
+    Write-Log -Level Verbose -Msg "$($json.id): Processing group: $($json.display_name)"
+    Write-Log -Level Verbose -Msg "$($json.id): Total Expressions found = $(($json.expression).count)"
+
+    foreach ($expression in $json.expression) {
+        if ($expression.member_type -eq 'VirtualMachine') {
+            $externalIdsToAdd = New-Object System.Collections.ArrayList
+            foreach ($external_id in ($expression.external_ids -split ' ')) {
+                Write-Log -Level Verbose -Msg "$($json.id): Processing external_id found in $groupFileIdentifier file: $external_id"
+                $vm = Get-VmFromInventory -Inventory $inventoryVms -ExternalId $external_id
+                if (-not ( $vm) ) {
+                    Write-Log -Level ERROR -Msg "$($json.id): External_id not found in NSX-T Inventory: $external_id"
+                    $missingExternalIds = $True
+                    $completionColor = "Red"
+                }
+                else {
+                    Write-Log -Level Verbose -Msg "$($json.id): Found matching external_id in NSX-T Inventory: $external_id ($($vm.display_name))"
+                    $externalIdsToAdd.Add($external_id) | Out-Null
+                }
+            }        
+        }
+    }
+
+    # If there are external_ids which have been found in NSX Manager, now we 
+    # patch the policy group with the updated list of external_ids.
+    if ($externalIdsToAdd.count -gt 0) {
+        $requiresPatch = $False
+        Write-Log -Level Verbose -Msg "$($json.id): Additional external_ids to add = $($externalIdsToAdd.count)"
+        try {
+            Write-Log -Level Verbose -Msg "$($json.id): Retrieving group configuration"
+            $group = Get-NsxtPolicyGroup -GroupId $json.id
+        }
+        catch {
+            Write-Host -Foregroundcolor Red "FAILED"
+            Write-Log -Level Error -Msg "$($json.id): Failed to retrieve group from NSX Manager $policyPath"
+            Write-Log -Level Error -Msg $_
+            $completionColor = "Red"
+            Continue
+        }
+        # Check to see if the inventoryVM already has the expression property or not
+        if (-not ($group | Get-Member -MemberType Properties -Name expression)) {
+            Write-Log -Level Verbose -Msg "$($json.id): Adding expression property"
+            $expressionProperty = New-Object System.Collections.ArrayList
+            $group | Add-Member -MemberType NoteProperty -Name 'expression' -Value $expressionProperty | Out-Null
+        }
+        $externalIdVmExpression = $group.expression | Where-Object {($_.resource_type -eq 'ExternalIDExpression') -AND ($_.member_type -eq 'VirtualMachine')}
+        if ($externalIdVmExpression) {
+            Write-Log -Level Verbose -Msg "$($json.id): Found existing VirtualMachine/ExternalIDExpression"
+            # Existing expression so we just append to the list
+            foreach ($externalIdToAdd in $externalIdsToAdd) {
+                if ($externalIdVmExpression.external_ids -notContains $externalIdToAdd) {
+                    Write-Log -Level Verbose -Msg "$($json.id): Adding external_id $externalIdToAdd"
+                    $externalIdVmExpression.external_ids += $externalIdToAdd
+                    $requiresPatch = $True
+                }
+                else {
+                    Write-Log -Level Verbose -Msg "$($json.id): External ID already exists in expression: $externalIdToAdd"
+                }
+            }
+        }
+        else {
+            Write-Log -Level Verbose -Msg "$($json.id): No existing VirtualMachine/ExternalIDExpression found"
+            # no existing expression so we need to add one.
+
+            # however need to figure out if we need to add a conjunctor or not first
+            if ($group.expression.count -gt 0) {
+                Write-Log -Level Verbose -Msg "$($json.id): Adding Conjunction with OR operator"
+                $group.expression += $(New-GroupConjunctionExpression -operator OR)
+            }
+
+            # Now lets add the create the externalId expression
+            Write-Log -Level Verbose -Msg "$($json.id): Adding new VirtualMachine/ExternalIDExpression"
+            $newExpression = New-GroupVmExpression -Id $externalIdsToAdd
+            $group.expression += $newExpression
+            $requiresPatch = $True
+        }
+
+        $policyPath = "/infra/domains/default/groups/$($json.id)"
+
+        try {
+            if ($requiresPatch -eq $True) {
+                Write-Log -Level info -Msg "$($json.id): Patching updated configuration"
+                Invoke-PatchPolicyObject -path $policyPath -Body $($group | ConvertTo-Json -depth 100) | Out-Null
+                if ($missingExternalIds -eq $True) {
+                    Write-Host -Foregroundcolor Green -NoNewLine "UPDATED "
+                    Write-Host -Foregroundcolor Yellow "(Some external_ids missing, check log file)"
+                }
+                else {
+                    Write-Host -Foregroundcolor Green "UPDATED"
+                }
+                Continue
+
+            }
+            else {
+                Write-Log -Level Verbose -Msg "$($json.id): No updates needing to be patched"
+                if ($missingExternalIds -eq $True) {
+                    Write-Host -Foregroundcolor Green -NoNewLine "OK "
+                    Write-Host -Foregroundcolor Yellow "(Some external_ids missing, check log file)"
+                }
+                else {
+                    Write-Host -Foregroundcolor Green "OK"
+                }
+                Continue
+            }
+        }
+        catch {
+            Write-Log -Level Error -Msg "$($json.id): Failed to patch object $policyPath"
+            Write-Log -Level Error -Msg "$($json.id): body = $($group | ConvertTo-Json -depth 100 -Compress)"
+            Write-Log -Level Error -Msg $_
+            Write-Host -Foregroundcolor Red "FAILED: Error patching object $policyPath"
+            $completionColor = "Red"
+            Continue
+        }
+    }
+    Write-Host -Foregroundcolor Cyan "SKIPPED"
+}
+$ElapsedTime = $((get-date) - $StartTime)
+Write-Log -Level Info -Msg "Script duration = $ElapsedTime"
+write-host -foregroundcolor Green "`nExecution completed in $($ElapsedTime.Hours):$($ElapsedTime.Minutes):$($ElapsedTime.Seconds)"
+Write-Host -foregroundcolor $completionColor "LogFile: $Script:LogFileName`n"
