123bda4f5d47a46912b9ad9b660861cad904432c
Dale Coghlan <dcoghlan@vmware.com>
2020-06-16 13:31:59 +1000

    Fixed formatting

--- NSX-V/Capture-VM-IDs/capture-vm-ids.ps1
+++ NSX-V/Capture-VM-IDs/capture-vm-ids.ps1
@@ -4,7 +4,7 @@ foreach ($vm in Get-VM) {
 	$vmHash = @{
         "name" = $vm.name;
         "uuid" = $vm.extensiondata.config.uuid;
-		"instanceuuid" = $vm.extensiondata.config.instanceuuid;
+        "instanceuuid" = $vm.extensiondata.config.instanceuuid;
         "ipaddress" = $vm.guest.ipaddress
 	}
 	$dataHash.Add($vm.extensiondata.moref.value, $vmHash)
